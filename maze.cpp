f29610f5c37ebab43342d611a20dbd50f9623002
駿 齋藤 <240063@jc-21.jp>
2025-11-28 13:35:06 +0900

    迷路ぽくはなっているが所々通路が途切れている部分があるからそこの修正をする

--- maze.cpp
+++ maze.cpp
@@ -7,10 +7,10 @@ namespace
 {
 	const int MapLow = 21;
 	const int MapColom = 11;
-	const int DrawSize = 20;
+	const int DrawSize = 25;
 	int map[MapLow][MapColom];
 	const int wall = 1;
-	const int load = 0;
+	const int Load = 0;
 }
 maze::maze()
 {
@@ -38,9 +38,10 @@ void maze::Draw()
 	int WallColor = GetColor(0, 255, 0);
 	int LoadColor = GetColor(255, 255, 255);
 	int GoalColor = GetColor(255, 0, 0);
-
+	int StartColor = GetColor(0, 0, 255);
 	int x_offset = (WIN_WIDTH - MapColom * DrawSize) / 2;
 	int y_offset = (WIN_HEIGHT - MapLow * DrawSize) / 2;
+
 	for (int y = 0; y < MapLow; y++)
 	{
 		for (int x = 0; x < MapColom; x++)
@@ -54,11 +55,15 @@ void maze::Draw()
 			{
 				DrawBox(x1, y1, x2, y2, WallColor, TRUE);
 			}
-			else
+			else if(map[y][x] == Load)
 			{
 				DrawBox(x1, y1, x2, y2, LoadColor, TRUE);
 			}
 			if (x == 0 && y == 1)
+			{
+				DrawBox(x1, y1, x2, y2, StartColor, TRUE);
+			}
+			if (x == 9 && y == 20)
 			{
 				DrawBox(x1, y1, x2, y2, GoalColor, TRUE);
 			}
@@ -73,16 +78,17 @@ void maze::CreateMaze()
 	Initialize();
 	for (int y = 1; y < MapLow - 1; y += 2)
 	{
+		//’Ê˜H‚Ì”z—ñ
 		std::vector<std::pair<int, int>> run_set;
 		for (int x = 1; x < MapColom - 1; x += 2)
 		{
-			map[y][x] = load;
+			map[y][x] = Load;
 			run_set.push_back({ y,x });
 			if (x < MapColom - 2 && y < MapLow - 2)
 			{
 				if (rand() % 2 == 0)
 				{
-					map[y][x + 1] = load;
+					map[y][x + 1] = Load;
 					continue;
 				}
 			}
@@ -94,7 +100,7 @@ void maze::CreateMaze()
 				int north_x = run_set[index].second;
 				if (north_y > 1)
 				{
-					map[north_y - 1][north_x] = load;
+					map[north_y - 1][north_x] = Load;
 				}
 			}
 			run_set.clear();
